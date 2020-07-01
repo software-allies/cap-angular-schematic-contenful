5f9ff6dfc4999834279f2933ccb8a911d43e1526
Diego SanMo <diego14290764@itcg.edu.mx>
2020-07-01 15:44:10 -0500

    Adding the logic to install and write the module configuration into the app.module

--- src/cap-angular-schematic-contentful/index.ts
+++ src/cap-angular-schematic-contentful/index.ts
@@ -1,103 +1,120 @@
-import { strings } from '@angular-devkit/core';
-import { 
-  apply,
-  template,
+import {
   branchAndMerge,
   chain,
-  forEach,
-  FileEntry,
-  move,
   Rule,
-  mergeWith,
-  SchematicsException,
   Tree,
-  url
- } from '@angular-devkit/schematics';
+  SchematicContext
+} from '@angular-devkit/schematics';
 import { Schema as ContentfulOptions } from './schema';
-import { 
-  addEnvironmentVar
-} from './cap-utils';
-import { 
-  getAppName
-} from './cap-utils/package';
 import { FileSystemSchematicContext } from '@angular-devkit/schematics/tools';
-import { getWorkspace } from '@schematics/angular/utility/config';
-import {
-  // buildRelativePath, 
-  findModule, 
-  MODULE_EXT, 
-  ROUTING_MODULE_EXT
-} from '@schematics/angular/utility/find-module';
-import { parseName } from '@schematics/angular/utility/parse-name';
-import { buildDefaultPath } from '@schematics/angular/utility/project';
-import { getProjectFromWorkspace } from '@angular/cdk/schematics/utils/get-project';
+import { NodeDependencyType } from 'schematics-utilities';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+import * as cap_utilities from 'cap-utilities';
+
+export function installPackageJsonDependencies(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    context.logger.log('info', `🔍 Installing packages...`);
+    return host;
+  };
+}
+
+// function addToEnvironments(options: ContentfulOptions): Rule {
+//   return (host: Tree) => {
+//     cap_utilities.addEnvironmentVar(host, [
+//       {
+//         env: '',
+//         appPath: options.path || '/src',
+//         key: 'contAccessToken',
+//         value: options.contAccessToken
+//       },
+//       {
+//         env: '',
+//         appPath: options.path || '/src',
+//         key: 'contSpace',
+//         value: options.contSpace
+//       },
+//       {
+//         env: '',
+//         appPath: options.path || '/src',
+//         key: 'contentfulEnvironment',
+//         value: options.contentfulEnvironment
+//       },
+//       {
+//         env: 'prod',
+//         appPath: options.path || '/src',
+//         key: 'contAccessToken',
+//         value: options.contAccessToken
+//       },
+//       {
+//         env: 'prod',
+//         appPath: options.path || '/src',
+//         key: 'contSpace',
+//         value: options.contSpace
+//       },
+//       {
+//         env: 'prod',
+//         appPath: options.path || '/src',
+//         key: 'contentfulEnvironment',
+//         value: options.contentfulEnvironment
+//       },
+//     ])
+//   }
+// }
+
+
+function installDependencies() {
+  return (host: Tree) => {
+    cap_utilities.addPackageToPackageJson(host, [{
+      type: NodeDependencyType.Default,
+      pkg: 'cap-angular-contentful',
+      version: '~0.1.4'
+    }])
+  }
+}
 
 
 
-
-function addToEnvironments(options: ContentfulOptions): Rule {
-    return (host: Tree) => {
-        addEnvironmentVar(host, '', options.path || '/src', 'contAccessToken', options.contAccessToken);
-        addEnvironmentVar(host, '', options.path || '/src', 'contSpace', options.contSpace);
-        addEnvironmentVar(host, '', options.path || '/src', 'contentfulEnvironment', options.contentfulEnvironment);
-        addEnvironmentVar(host, 'prod', options.path || '/src', 'contAccessToken', options.contAccessToken);
-        addEnvironmentVar(host, 'prod', options.path || '/src', 'contSpace', options.contSpace);
-        addEnvironmentVar(host, 'prod', options.path || '/src', 'contentfulEnvironment', options.contentfulEnvironment);
-    }
+function addToRootModule(options: ContentfulOptions) {
+  return (host: Tree) => {
+    cap_utilities.addToNgModule(host, options, [
+      {
+        name: 'CapContentfulModule',
+        path: 'cap-angular-contentful',
+        type: 'module',
+        forRootValues: {
+          params: [
+            {
+              name: 'space_id',
+              value: `${options.contSpace}`,
+            },
+            {
+              name: 'environment',
+              value: `${options.contentfulEnvironment}`
+            },
+            {
+              name: 'delivery_accessToken',
+              value: `${options.contAccessToken}`
+            }
+          ]
+        }
+      }
+    ])
+  }
 }
 
 export function capAngularSchematicContentful(options: any): Rule {
   return (tree: Tree, context: FileSystemSchematicContext) => {
 
-    // Get project
-    options.project = (options.project) ? options.project : getAppName(tree);
-    if (!options.project) {
-      throw new SchematicsException('Option "project" is required.');
-    }
-
-    const workspace = getWorkspace(tree);
-    const project: any = getProjectFromWorkspace(workspace, options.project);
-    if (!project) {
-      throw new SchematicsException(`Project is not defined in this workspace.`);
-    }
-
-    if (options.path === undefined) {
-      options.path = buildDefaultPath(project);
-    }
-    
-    options.module = findModule(tree, options.path, 'app' + MODULE_EXT, ROUTING_MODULE_EXT);
-    options.name = '';
-    const parsedPath = parseName(options.path!, options.name);
-    options.name = parsedPath.name;
-    options.path = parsedPath.path;
-    
-
-    const projectType: string = project.projectType || project.projects[options.project].projectType;
-    if (projectType !== 'application') {
-      throw new SchematicsException(`Is required a project type of "application".`);
-    }
-
-    // Object that will be used as context for the EJS templates.
-    const baseTemplateContext = {
-      ...strings,
-      ...options,
-    };
-
-    const templateSource = apply(url('./files'), [
-      template(baseTemplateContext),
-      move(null as any, parsedPath.path),
-      forEach((fileEntry: FileEntry) => {
-        if (tree.exists(fileEntry.path)) {
-          tree.overwrite(fileEntry.path, fileEntry.content);
-        }
-        return fileEntry;
-      })
-    ]);
+    cap_utilities.setupOptions(tree, options)
 
     return chain([
       branchAndMerge(chain([
-        addToEnvironments(options),
-        mergeWith(templateSource)
+        installDependencies(),
+        installPackageJsonDependencies(),
+        // addToEnvironments(options),
+        addToRootModule(options)
       ])),
     ])(tree, context);
 
